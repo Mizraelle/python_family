cf0bed04fa1922b5bb1d860e4c07de9a1ba42929
Bogdankozlovskiy <bkozlovskiy@bk.ru>
2019-06-03 17:36:44 +0000

    server commit

--- tsoy/settings.py
+++ tsoy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0vr-eqn_o0q#7&w@79$+po!uf!bt^s!&kz36j^t1mnug4w5(u#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['asyncio.pythonanywhere.com']
 
 
 # Application definition
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
